329cc595a327b4980d51fddb95cdd15df199866f
Abdul Latif Mubasir <bj.angel119@gmail.com>
2022-04-17 09:17:04 +0700

    create interfaces

---
+++ src/interfaces/user.ts
@@ -0,0 +1,10 @@
+export interface IUser {
+  id      : number,
+  name    : string,
+  username: string,
+  email   : string,
+  address : Object,
+  phone   : string,
+  website : string,
+  company : string,
+}
